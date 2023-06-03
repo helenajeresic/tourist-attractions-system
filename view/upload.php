0d6fd67bfe012e46d2cbd84eb0d9a0a762416d08
root <root@DESKTOP-9OGM0GT>
2023-06-04 00:03:24 +0200

    view za upload

--- view/upload.php
+++ view/upload.php
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload</title>
-</head>
-<body>
-    <form action="<?php echo __SITE_URL . 'index.php?rt=upload/processUpdate'?>" method="post" enctype="multipart/form-data">
+<?php require_once __SITE_PATH . '/view/_header.php';?>
+<form action="<?php echo __SITE_URL . 'index.php?rt=upload/processUpdate'?>" method="post" enctype="multipart/form-data">
+    <div>
+        <label for="name">Naziv atrakcije:</label>
+        <input type="text" name="name" id="name">
+    </div>
+    
+    <div>
+        <label for="name">Opis atrakcije:</label>
+        <textarea name="description" id="description"></textarea>
+    </div>
+
+    <label for="xcoordinate">x koordinata:</label>
+    <input type="text" name="xcoordinate" id="xcoordinate">
+    </div>
+
+    <div>
+    <label for="ycoordinate">y koordinata:</label>
+    <input type="text" name="ycoordinate" id="ycoordinate">
+
+    <div> 
         <input type="file" name="file">
         <input type="submit" value="Upload">
-    </form>
-</body>
-</html>
+    </div>
+
+    <input type="submit" value="Submit">
+</form>
+<?php require_once __SITE_PATH . '/view/_footer.php';?>
