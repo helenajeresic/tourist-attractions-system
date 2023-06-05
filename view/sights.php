40e2ffa6309c8b46d125179980346d63851ba1af
Matej <matejj.crnkovic@gmail.com>
2023-06-05 12:55:32 +0200

    small changes in gallery style

--- view/sights.php
+++ view/sights.php
@@ -6,12 +6,12 @@
                 <select name="lang[]" multiple class="form-control">
                  
                     <?php foreach( $data as $d ) { ?>
-                        <option value="<?php $d->__get('name')?>"><?php echo $d->__get('name')?></option>
+                        <option value="<?php echo $d->__get('_id')?>"><?php echo $d->__get('name')?></option>
                     <?php } ?>
                 </select>
 
                 <h3>Koju zelis da ti bude prva:</h3>
-                <select name="lang" multiple class="form-control">
+                <select name="lan" class="form-control">
                  
                     <?php foreach( $data as $d ) { ?>
                         <option value="<?php $d->__get('name')?>"><?php echo $d->__get('name')?></option>
@@ -39,60 +39,3 @@
 <?php } ?>
 
 <?php require_once __SITE_PATH . '/view/_footer.php';?>
-
-<!--<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Multiple Select Dropdown in PHP</title>
-
-        <style>
-            .myform-container{
-                width: 600px;
-                margin: 50px auto;
-            }
-
-            .myform-container select{
-                width: 300px;
-            }
-        </style>
-
-    </head>
-    <body>
-
-        <div class="myform-container">
-
-
-            <form action="" method="post" class="mb-3">
-                <h3>Choose your favorite languages</h3>
-                <select name="lang[]" multiple class="form-control">
-                    <option value=""disabled selected>Choose option</option>
-                    <option value="Laravel">Laravel</option>
-                    <option value="Php">Php</option>
-                    <option value="Jquery">Jquery</option>
-                    <option value="Node Js">NodeJs</option>
-                    <option value="Bootstrap">Bootstrap</option>
-                </select>
-
-                <div>
-                    <br>
-                    <input type="submit" name="submit" vlaue="Choose options">
-                </div>
-
-            </form>
-
-            <?php
-            /*if (isset($_POST['submit'])) {
-                if (!empty($_POST['lang'])) {
-                    foreach ($_POST['lang'] as $selected) {
-                        echo '<p class="select-tag mt-3">' . $selected . '</p>';
-                    }
-                } else {
-                    echo '<p class="error alert alert-danger mt-3">Please select any value</p>';
-                }
-            }*/
-            ?>
-
-        </div>
-
-    </body>
-</html>-->
