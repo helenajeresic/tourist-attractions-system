0632105e75a5072cbd9e977ed99696fd09b37d74
root <root@DESKTOP-9OGM0GT>
2023-06-07 15:48:32 +0200

    go back btn added

--- view/login.php
+++ view/login.php
@@ -10,27 +10,27 @@ if (isset($_SESSION['error'])) {
 
 <form method="post" action="<?php echo __SITE_URL . 'index.php?rt=login/processLoginForm' ?>">
         <div class="form-group">
-            <label for="username">Username:</label>
-            <input class="form-control" id="username" name="username" type="text" required>
+            <label for="username">Korisničko ime</label><br>
+            <input class="form-control" id="username" name="username" type="text" placeholder="Korisničko ime.." required>
         </div>
         <br>
         <div class="form-group">
-            <label for="password">Password:</label>
-            <input class="form-control" id="password" name="password" type="password" required>
+            <label for="password">Lozinka</label><br>
+            <input class="form-control" id="password" name="password" type="password" placeholder="Lozinka.." required>
         </div>
         <br>
         <div class="float-end">
-            <input class="btn btn-primary" type="submit" name="login" value="Login"/>
+            <input class="btn btn-primary" type="submit" name="login" value="Prijavi se"/>
         </div>
         <br>
 </form>
 
 <div class="form-group">
-            <label>First time here?</label>
+            <label>Registriraj se</label>
             <form method="post" action="<?php echo __SITE_URL . 'index.php?rt=login/register' ?>">
             <div class="float-end">
             <br>
-            <input class="btn btn-secondary" type="submit" name="register" value="Register"/>
+            <input class="btn btn-secondary" type="submit" name="register" value="Registracija"/>
         </div>
 </div>
 
--- view/register.php
+++ view/register.php
@@ -1,23 +1,31 @@
 <?php require_once __SITE_PATH . '/view/_header.php'; ?>
 
 <form action="<?php echo __SITE_URL . 'index.php?rt=login/processRegister' ?>" method="post">		
-    <label for="fname">Ime</label><br />
-    <input type="text" id="fname" name="first_name" placeholder="Vaše ime.." required>
-    <br />
-    <label for="lname">Prezime</label><br />
-    <input type="text" id="lname" name="last_name" placeholder="Vaše prezime.." required>
-    <br />
-    <label for="rusername">Korisničko ime</label><br />
-    <input type="text" id="rusername" name="username" placeholder="Korisničko ime.." required>
-    <br />
-    <label for="rpassword">Lozinka</label><br />
-    <input type="password" id="rpassword" name="password" placeholder="Vaša lozinka.." required>
-    <br />
-    <label for="lmail">e-mail</label><br />
-    <input type="email" id="lmail" name="email" placeholder="Vaš e-mail.." required>
-    <br />
-    <br />
+    <label for="fname">Ime</label><br>
+    <input type="text" id="fname" name="first_name" placeholder="Vaše ime.." required><br>
+    <br>
+    <label for="lname">Prezime</label><br>
+    <input type="text" id="lname" name="last_name" placeholder="Vaše prezime.." required><br>
+    <br>
+    <label for="rusername">Korisničko ime</label><br>
+    <input type="text" id="rusername" name="username" placeholder="Korisničko ime.." required><br>
+    <br>
+    <label for="rpassword">Lozinka</label><br>
+    <input type="password" id="rpassword" name="password" placeholder="Vaša lozinka.." required><br>
+    <br>
+    <label for="lmail">e-mail</label><br>
+    <input type="email" id="lmail" name="email" placeholder="Vaš e-mail.." required><br>
+    <br>
+    <br>
     <input type="submit" name="submit" value="Pošalji" />
 </form>
+
+<div class="form-group">
+            <form method="post" action="<?php echo __SITE_URL . 'index.php?rt=login' ?>">
+            <div class="float-end">
+            <br>
+            <input class="btn btn-secondary" type="submit" name="register" value="Vrati se na prijavu"/>
+        </div>
+</div>
 
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
