4059a2992f41582709c66bc05e0be4f51fb6de88
Matej <matejj.crnkovic@gmail.com>
2023-06-07 21:33:14 +0200

    tipfeler changed

--- view/welcome.php
+++ view/welcome.php
@@ -7,7 +7,7 @@
         Kako bi u što kraćem vremenu mogli obići željene atrakcije SIGHTS2SEE će vam predložiti <br>
         redoslijed obilaska atrakcija koje u tom redoslijedu tvore najkraći put od odabrane točke. <br><br>
 
-        Ukoliko niste odabrali sve željene lokalitete ili pak želite kreunti od neke druge atrakcije nema problema. <br>
+        Ukoliko niste odabrali sve željene lokalitete ili pak želite krenuti od neke druge atrakcije nema problema. <br>
         SIGHTS2SEE možete koristiti koliko vas je volja, ali nemojte previše visiti na internetu <br>
         Vrijeme je za obilazak grada! <br>
         <b>SIGHTS2SEE
